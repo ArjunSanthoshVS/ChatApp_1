9f5a38b76f2ac36c3e7a0c4b59fc946393f3d608
Arjun Santhosh <arjunsanthosh738@gmail.com>
2023-12-29 17:11:22 +0530

    Ip issues solving 2

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', async function () {
-    // const BASE_URL = "https://chat-service-fhbc.onrender.com";
-    const BASE_URL = "http://localhost:3000";
+    const BASE_URL = "https://chat-service-fhbc.onrender.com";
+    // const BASE_URL = "http://localhost:3000";
 
     //Initializing all variables
     const socket = io();
